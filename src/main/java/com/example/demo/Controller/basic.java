110c14213424fa90414b9b919ac802bc2a27a5e5
bala-amypo <bala@amypo.in>
2025-12-17 08:21:42 +0000

    User commit

--- src/main/java/com/example/demo/Controller/basic.java
+++ src/main/java/com/example/demo/Controller/basic.java
@@ -1,6 +1,6 @@
 package com.example.demo.Controller;
 import org.springframework.web.bind.annotation.*;
-@Rest Controller
+@RestController
 public class basic{
     @GetMapping("\home")
     public String name(){
--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import java.util.List;
 
-@SwaggerConfig
+@Configuration
 public class SwaggerConfig {
 
     @Bean
